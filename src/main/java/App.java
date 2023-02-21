ddc71d7235c7e5f35b64433341604634783b0ee1
Jordan-Conway <D00244954@student.dkit.ie>
2023-02-21 12:48:49 +0000

    Args are now stored in variables

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,15 @@
 public class App {
 
     public static void main(String[] args) {
+        String source = args[0];
+        String destination = args[1];
+
         App app = new App();
-        app.start(args);
+        app.start(source, destination);
     }
 
-    public void start(String[] args){
-        final BackupMaker backupMaker = new BackupMaker(args[0], args[1]);
+    public void start(String source, String destination){
+        final BackupMaker backupMaker = new BackupMaker(source, destination);
         Thread quitHook = new Thread(() -> {
             System.out.println("Triggered quit hook");
             backupMaker.interrupt();
